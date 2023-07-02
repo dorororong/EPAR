e32686e750c943d2162dc4836df0cad9525eaca2
dorororong <ib25888@gmail.com>
2023-07-02 20:38:56 +0900

    commit

--- EPAR/settings.py
+++ EPAR/settings.py
@@ -15,12 +15,32 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import os, json
+from django.core.exceptions import ImproperlyConfigured
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+secret_file = os.path.join(BASE_DIR, 'secret.json')  # secrets.json 파일 위치를 명시
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+
+SECRET_KEY = get_secret("SECRET_KEY")
+
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m3@e9h#z#@2$enj*!4ug=g$j4i(lqdvde#l)27hel8#(w4%gc)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
