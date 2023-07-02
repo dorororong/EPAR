558e4bf99a970297e80ebdf5a66d32bee0b0385c
dorororong <105921988+dorororong@users.noreply.github.com>
2023-07-02 19:36:41 +0900

    Update settings.py
    key deleted

--- EPAR/settings.py
+++ EPAR/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m3@e9h#z#@2$enj*!4ug=g$j4i(lqdvde#l)27hel8#(w4%gc)'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
